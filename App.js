0f3d7e37fa799078600dbe05c7abd35013bd8147
thiagofeldner <tfeldner@hotmail.com>
2022-07-26 12:41:24 -0300

    Primeiro commit do projeto

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { THEME } from "./src/styles/theme";
+import { Routes } from './src/routes';
+import { Loading } from "./src/components/Loading";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+    return (<NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </NativeBaseProvider>);
+}
---
+++ src/screens/Details.tsx
@@ -0,0 +1,156 @@
+import React, { useEffect, useState } from 'react';
+import { VStack, Text, HStack, useTheme, ScrollView, Box } from 'native-base';
+
+import { useNavigation, useRoute } from '@react-navigation/native';
+import  firestore from '@react-native-firebase/firestore';
+import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO';
+import { CircleWavyCheck, Hourglass, DesktopTower, ClipboardText } from 'phosphor-react-native';
+
+import { dateFormat } from '../utils/firestoreDateFormat';
+
+import { Header } from '../components/Header';
+import { OrderProps } from '../components/Order';
+import { Loading } from '../components/Loading';
+import { CardDetails } from '../components/CardDetails';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+import { Alert } from 'react-native';
+
+type RouteParams = {
+  orderId: string;
+}
+
+type OrderDetails = OrderProps & {
+  description: string;
+  solution:string;
+  closed: string;
+}
+
+export function Details() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [solution, setSolution] = useState('');
+  const [order, setOrders] = useState<OrderDetails>({} as OrderDetails);
+
+  const navigaton = useNavigation();
+  const { colors } = useTheme();
+  const route = useRoute();
+  const {orderId} = route.params as RouteParams;
+
+  function handleOrderClose(){
+    if(!solution){
+      return Alert.alert('Solicitação', 'Informe a solução para encerrar o chamado')
+    }
+
+    firestore()
+    .collection<OrderFirestoreDTO>('orders')
+    .doc(orderId)
+    .update({
+      status: 'closed',
+      solution,
+      closed_at: firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => {
+      Alert.alert('Solicitação', 'Chamado encerrado.')
+      navigaton.goBack();
+    })
+    .catch((e) => {
+      console.log(e);
+      Alert.alert('Solicitação', 'Não foi possível encerrar o chamado.');
+    });
+  }
+
+  useEffect(() => { 
+    firestore()
+    .collection<OrderFirestoreDTO>('orders')
+    .doc(orderId)
+    .get()
+    .then((doc) => {
+      const { patrimony, description, status, created_at, closed_at, solution } = doc.data();
+
+      const closed = closed_at ? dateFormat(closed_at) : null;
+
+      setOrders({
+        id: doc.id,
+        patrimony,
+        description,
+        status,
+        solution,
+        when: dateFormat(created_at),
+        closed
+      });
+
+      setIsLoading(false);
+    });
+  }, [])
+
+  if(isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Box px={6} bg="gray.600">
+        <Header title="Solicitação" />
+      </Box> 
+      <HStack bg="gray.500" justifyContent="center" p={4}>
+        {
+          order.status === 'closed'
+          ? <CircleWavyCheck size={22} color={colors.green[300]} />
+          : <Hourglass size={22} color={colors.secondary[700]} />
+        }
+
+        <Text 
+          fontSize="sm"
+          color={order.status === 'closed' ? colors.green[300] : colors.secondary[700] }
+          ml={2}
+          textTransform="uppercase"
+        >
+          {order.status === 'closed' ? 'Finalizado' : 'Em andamento'}
+        </Text>
+      </HStack>
+
+      <ScrollView mx={5} showsVerticalScrollIndicator={false}>
+        <CardDetails 
+          title='Equipamento'
+          description={`Patrimônio ${order.patrimony}`}
+          icon={DesktopTower}
+          
+        />
+
+        <CardDetails 
+          title='Descrição do Problema'
+          description={order.description}
+          icon={ClipboardText}
+          footer = {`Registrado em ${order.when}`}
+        />
+       
+        <CardDetails 
+          title='Solução'
+          icon={CircleWavyCheck}
+          description={order.solution}
+          footer={order.closed && `Encerrado em ${order.closed}`}
+        >
+          {
+            order.status === 'open' &&
+            <Input 
+              placeholder='Decrição da Solução'
+              onChangeText={setSolution}
+              textAlignVertical="top"
+              multiline
+              h={24}
+            />
+          }
+        </CardDetails>
+      </ScrollView>
+
+      {
+        order.status === 'open' && 
+        <Button 
+          title='Encerrar chamado'
+          m={5}
+          onPress={handleOrderClose}
+        />
+      }
+    </VStack>
+  );
+}
